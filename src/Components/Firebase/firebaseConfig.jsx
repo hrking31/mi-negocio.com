505245533283ea3eeeaa7cb0b230428a8e84c0a7
Hernando Rey <hrking31@gmail.com>
2025-02-08 17:25:54 -0500

    se modifica el nombre de la carpeta firebaseconfig para poder agregarla a gitignore

--- src/Components/Firebase/firebaseConfig.jsx
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyB_gKsa8U9kGnyvuKZYqPx17dio_PszZoY",
-  authDomain: "mi-negocio-30949.firebaseapp.com",
-  projectId: "mi-negocio-30949",
-  storageBucket: "mi-negocio-30949.appspot.com",
-  messagingSenderId: "284878352366",
-  appId: "1:284878352366:web:b649824a6b60f0bee828ff",
-};
-export default firebaseConfig;
